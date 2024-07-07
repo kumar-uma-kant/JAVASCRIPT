bd99e8682c7fdef9e7bdb8ad26aca8f5bd107bb6
kumar-uma-kant <umakantkumar8759@gmail.com>
2024-07-07 15:55:35 +0530

    .

--- 05_iteration/eight.js
+++ 05_iteration/eight.js
@@ -5,8 +5,10 @@ const myNums = [1, 2, 3]
 // const myTotal = myNums.reduce(function (acc, currval) { // acc means 'accumulator, & currval means 'Current value'
 //     console.log(`acc: ${acc} and currval: ${currval}`);
 //     return acc + currval
-// }, 0)
-// expresing in Arrow function
+// }, 0)     // yaha 0 representing ki accumulator ki starting value 0 hai
+
+// expresing in Arrow function👇
+
 const myTotal = myNums.reduce( (acc, curr) => acc+curr, 0)
 
 console.log(myTotal);
